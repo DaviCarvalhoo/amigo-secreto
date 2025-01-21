48dd0a4963b8cf7a118041a103e4f73c6342f173
DaviCarvalhoo <davicarvalhotech@gmail.com>
2025-01-21 14:46:39 -0300

    add function`adicionarAmigo`

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+const inputAmigo = document.getElementById("amigo");
+const listaAmigos = document.getElementById("listaAmigos");
+const resultado = document.getElementById("resultado");
+let amigos = [];
+
+function adicionarAmigo() {
+  const nome = inputAmigo.value.trim();
+
+  if (nome === "") {
+    alert("Por favor, digite um nome.");
+  }
+  amigos.push(nome);
+
+  const itemLista = document.createElement("li");
+  itemLista.textContent = nome;
+  listaAmigos.appendChild(itemLista);
+
+  inputAmigo.value = "";
+}
